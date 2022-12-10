8737e00ff6d8cd799dd44b54cfd64320a2783c32
EricoRibeiro <ericor25@gmail.com>
2022-12-10 18:55:04 -0300

    azure connection

--- Program.cs
+++ Program.cs
@@ -1,6 +1,53 @@
+using System.Text.RegularExpressions;
+using Azure.Storage.Blobs;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello my change is online!");
+var connectionString = 
+    app.Configuration.GetConnectionString("DefaultConnection");
+
+var azureStorageConectionString = 
+    app.Configuration.GetValue<string>("AzureStorageConnectionString");
+
+var secrets = new Secrets(); 
+    app.Configuration.GetSection("Secrets").Bind(secrets);
+
+app.MapGet("/", () => new {
+    ConnectionString = connectionString,
+    Secrets = secrets,
+    apiUrl = app.Configuration.GetValue<string>("ApiUrl")
+});
+
+app.MapPost("/", (Upload model) => 
+{
+    UploadBase64Image(model.Image, "nome do container criado no Azure");
+    return Results.Ok();
+});
 
 app.Run();
+
+string UploadBase64Image(string base64Image, string container)
+{
+    var fileName = Guid.NewGuid().ToString() + ".jpg";
+    var data = new Regex(@"^data:image\/[a-z]+;base64,").Replace(base64Image, "");
+
+    byte[] imageBytes = Convert.FromBase64String(data);
+    var blobClient = new BlobClient(azureStorageConectionString, container, fileName);
+
+    using(var stream = new MemoryStream(imageBytes))
+    {
+        blobClient.Upload(stream);
+    }
+
+    return blobClient.Uri.AbsoluteUri;
+}
+
+public record Upload(string Image);
+
+public class Secrets
+{
+    public string JwtTokenSecret {get;set;} = String.Empty;
+    public string ApiKey {get;set;} = String.Empty;
+    public string PrivateKey {get;set;} = String.Empty;
+}
